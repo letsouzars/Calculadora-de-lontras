053dd361ffee7587ea9499c3974803064926f707
Leticia Souza <133662327+letsouzars@users.noreply.github.com>
2023-09-26 16:52:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+class Calculadora {
+    constructor() {
+        this.valor = '0'
+    }
+
+
+    adicionarNumero(numero) {
+        if (this.valor === '0') {
+            this.valor = numero
+        } else {
+            this.valor += numero
+        }
+        this.atualizarVisor();
+    }
+
+
+    adicionarOperacao(operacao) {
+        if (this.valor.slice(-1) === '+' ||
+            this.valor.slice(-1) === '-' ||
+            this.valor.slice(-1) === '*' ||
+            this.valor.slice(-1) === '/') {
+            this.valor = this.valor.slice(0, -1) + operacao
+        } else {
+            this.valor += operacao
+        }
+        this.atualizarVisor()
+
+    }
+    adicionarResultado() {
+        try {
+            this.valor = eval(this.valor).toString()
+        } catch (e) {
+            this.valor = 'Erro'
+        }
+        this.atualizarVisor()
+    }
+
+
+
+    limpar() {
+        this.valor = '0';
+        this.atualizarVisor();
+    }
+    
+    atualizarVisor() {
+        document.getElementById('visor').value = this.valor
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+let calculadora = new Calculadora()
